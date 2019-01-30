4e2cd791e2c95edfcf99757d7e79139b00b2d348
Evgeny Kotov <dev@seniorcote.com>
2019-01-30 16:43:02 +0300

    Add UK locale

--- renderer/modules/i18n.js
+++ renderer/modules/i18n.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 import en from '../../resources/locales/en';
 import ru from '../../resources/locales/ru';
+import uk from '../../resources/locales/uk';
 
 Vue.use(VueI18n);
 
@@ -11,6 +12,7 @@ const i18n = new VueI18n({
     messages: {
         en,
         ru,
+        uk,
     },
 });
 
