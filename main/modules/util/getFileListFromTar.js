11c1acd6e4c2197d4d61d250ede67c4e48e59279
Evgeny Kotov <dev@seniorcote.com>
2019-01-30 11:39:30 +0300

    WIP

---
+++ main/modules/util/getFileListFromTar.js
@@ -0,0 +1,27 @@
+const tar = require('tar');
+const fs = require('fs');
+
+module.exports = function getTarGzInfo(path) {
+    const info = {
+        numberOfFiles: 0,
+        files: [],
+    };
+
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path)
+            .pipe(tar.t())
+            .on('entry', entry => {
+                if (entry.type !== 'File') {
+                    return;
+                }
+
+                info.numberOfFiles++;
+                info.files.push({
+                    name: entry.path.substr(entry.path.lastIndexOf('/') + 1),
+                    nameWithPath: entry.path,
+                });
+            })
+            .on('end', () => resolve(info))
+            .on('error', () => reject('Unable to read file list.'));
+    });
+};
